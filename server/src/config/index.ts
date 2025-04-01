9aa59d5add2c913fd46576e5f9bc9a4108af9935
Teodora Siljanoska <teodorasiljanoska@MacBookPro.home>
2025-04-01 16:30:09 +0200

    Strapi stellate plugin forked and rebuild as new plugin compatible with Strapi v5.

---
+++ server/src/config/index.ts
@@ -0,0 +1,16 @@
+export default {
+  default: {},
+  validator: (config) => {
+    if (!config.purgingAPIToken) {
+      throw new Error(
+        "purgingAPIToken needs to be set to your Stellate service's purging API token."
+      );
+    }
+
+    if (!config.serviceName) {
+      throw new Error(
+        "serviceName needs to be set to your Stellate service's name."
+      );
+    }
+  },
+};
